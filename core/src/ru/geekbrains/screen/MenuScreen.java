60113b21bb242a0917196a68a3333aac6fe7353c
Alexey Kutepov <alkutepov@gmail.com>
2019-01-19 11:24:54 +0300

    ДЗ 2

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -10,12 +10,17 @@ import ru.geekbrains.base.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
 
+    private static final float V_LEN = 2.5f;
+
     SpriteBatch batch;
     Texture img;
     Texture background;
 
     Vector2 pos;
     Vector2 v;
+    Vector2 touch;
+
+    Vector2 buf;
 
     @Override
     public void show() {
@@ -24,7 +29,9 @@ public class MenuScreen extends Base2DScreen {
         background = new Texture("bg.png");
         img = new Texture("badlogic.jpg");
         pos = new Vector2(0, 0);
-        v = new Vector2(1,1);
+        v = new Vector2(0,0);
+        touch = new Vector2(0, 0);
+        buf = new Vector2(0, 0);
     }
 
     @Override
@@ -32,13 +39,17 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0.5f, 0.2f, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        buf.set(touch);
+        if (buf.sub(pos).len() > V_LEN) {
+            pos.add(v);
+        } else {
+            pos.set(touch);
+        }
+
         batch.begin();
         batch.draw(background, 0, 0);
         batch.draw(img, pos.x, pos.y);
         batch.end();
-        if (Gdx.graphics.getWidth() - 256 > pos.x && Gdx.graphics.getHeight() - 256 > pos.y) {
-            pos.add(v);
-        }
     }
 
     @Override
@@ -56,6 +67,8 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         System.out.println("touchDown " + screenX + " " + (Gdx.graphics.getHeight() - screenY));
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos).setLength(V_LEN));
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
